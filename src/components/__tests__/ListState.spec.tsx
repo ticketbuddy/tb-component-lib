9ed158dbbe127a11bca0bcd0e8b6bbd5470654f3
James Bowers <james@james-bowers.me>
2020-04-11 14:43:09 +0100

    spacing in list state test

--- src/components/__tests__/ListState.spec.tsx
+++ src/components/__tests__/ListState.spec.tsx
@@ -9,12 +9,13 @@ const onPopulated = () => <p>List is NOT empty</p>
 describe("EnumState - list", () => {
   test("when list is empty", () => {
     const {getByText} = render(<EnumState e={[]} onEmpty={onEmpty} onPopulated={onPopulated} />)
+
     expect(getByText('List is empty')).toBeInTheDocument()
   })
 
   test("when list is has content", () => {
     const {getByText} = render(<EnumState e={["item"]} onEmpty={onEmpty} onPopulated={onPopulated} />)
-    //
+
     expect(getByText('List is NOT empty')).toBeInTheDocument()
   })
 })
@@ -22,12 +23,13 @@ describe("EnumState - list", () => {
 describe("EnumState - map", () => {
   test("when map is empty", () => {
     const {getByText} = render(<EnumState e={{}} onEmpty={onEmpty} onPopulated={onPopulated} />)
+
     expect(getByText('List is empty')).toBeInTheDocument()
   })
 
   test("when map is has content", () => {
     const {getByText} = render(<EnumState e={{a: "item"}} onEmpty={onEmpty} onPopulated={onPopulated} />)
-    //
+
     expect(getByText('List is NOT empty')).toBeInTheDocument()
   })
 })
